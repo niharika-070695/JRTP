b1e4e57802daedffdee7794cd21ddf443d1eb31b
Niharika Reddy Voodem <harika5991@gmail.com>
2024-12-06 00:04:40 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -6,9 +6,9 @@ class Application{
         int a = 60;
         int b = 90;
         int c = a*b;
-
-
-
+        int d = 90;
+        int e = 85;
+        int f = 40;
         System.out.println(c);
         System.out.println("This is application class");
         System.out.println("This is application class");
