1539fadc54bc7112731c7c0582798829c5f84df6
niharika0706 <harika5991@gmail.com>
2024-12-05 23:59:18 -0500

    commiting application.java file

--- Application.java
+++ Application.java
@@ -1,9 +1,10 @@
 
 class Application{
     public static void main(String... args){
-        int a = 5;
-        int b = 10;
-        int c = a+b;
+        System.out.println("Welcome to Canada");
+        int a = 60;
+        int b = 90;
+        int c = a*b;
         System.out.println(c);
         System.out.println("This is application class");
         System.out.println("This is application class");
