db2b73d3e1e3830912454b0df6b35d3b6830fc10
Niharika Reddy Voodem <harika5991@gmail.com>
2024-12-05 23:40:07 -0500

    Update Application.java

---
+++ Application.java
@@ -0,0 +1,7 @@
+class Application{
+  public static void main(String... args){
+    System.out.println("This is application class");
+    System.out.println("This is application class");
+    System.out.println("This is application class");
+  }
+}
